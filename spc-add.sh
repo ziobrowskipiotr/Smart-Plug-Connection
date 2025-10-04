d90e0bcbc337a1ba0875fd0f135c82dc6445f560
ziobrowskipiotr <piotrziobrowski.work@gmail.com>
2025-10-04 19:13:56 +0200

    Added validation of adding smartplug

--- spc-add.sh
+++ spc-add.sh
@@ -4,25 +4,56 @@
 # Source the helpers script
 source ./spc-helpers.sh
 
-if (!check_connection()); then
+if ! check_connection; then
   echo "Check your internet connection"
   exit 1
 fi
 
-# TO DO - Check if the format of name and IP address is correct
-read -p "Write name of the device: " name
-if (sqlite3 "$DB_FILE" "SELECT name FROM devices WHERE name = '$name';" | grep -q .); then
-  echo "Device with name \"$name\" is already in the database"
+# Get name of the device
+read -p "Write name of the device: " NAME
+
+# Check if the name is valid
+if ! validate_device_name "$NAME"; then
+  echo "Invalid device name."
+  exit 1
+fi
+
+# Check if the name is already in the database
+if sqlite3 "$DB_FILE" "SELECT name FROM devices WHERE name = '$NAME';" | grep -q .; then
+  echo "Device with name \"$NAME\" is already in the database"
   echo "Choose another name"
   exit 1
 fi
+
+# Get IP address of the device
 read -p "Write IP address of the device: " IP
-if (sqlite3 "$DB_FILE" "SELECT ipv4 FROM devices WHERE ipv4 = '$IP';" | grep -q .); then
+if ! validate_ipv4 "$IP"; then
+  echo "IP address is not in correct format"
+  echo "Correct format is x.x.x.x"
+  exit 1
+fi
+
+# Get IP address and mask of the connector
+connector_ip_and_mask=$(get_connector_ip_and_mask)
+if [ $? -ne 0 ]; then
+  echo "ERROR: Failed to get the connector IP address and mask"
+  exit 1
+fi
+
+# Check if the IP address is in the same subnet as the connector
+if ! is_ip_in_same_subnet "$IP" "$connector_ip_and_mask"; then
+  echo "IP address \"$IP\" is not in the same subnet as the connector"
+  exit 1
+fi
+
+# Check if the IP address is already in the database
+if sqlite3 "$DB_FILE" "SELECT ipv4 FROM devices WHERE ipv4 = '$IP';" | grep -q .; then
   echo "Device with IP address \"$IP\" is already in the database"
   echo "Choose another IP address"
   exit 1
 fi
 
+# Check if the device is available
 STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://"$IP") 
 if (( STATUS_CODE!=200 )); then
   echo "IP address of smartplug is unavailable"
@@ -31,6 +62,7 @@ if (( STATUS_CODE!=200 )); then
 fi
 
 # Check MAC address of the device
+ping -c 1 -W 1 "$IP" &> /dev/null # Send a single ping request with a 1 second timeout
 MAC_ADDRESS=$(ip neigh show "$IP" | awk '{print $5}')
 if [ -z "$MAC_ADDRESS" ]; then
   echo "Failed to retrieve MAC address for IP $IP"
